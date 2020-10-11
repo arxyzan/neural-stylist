9f5b10c6f3a6a61ce2c8bccce84ba340749f341c
xavier.chekhov <xavier.chekhov@gmail.com>
2020-10-11 17:28:38 +0330

    image resize

--- app/styler/utils.py
+++ app/styler/utils.py
@@ -7,10 +7,11 @@ def read_image(image_path, as_4d_tensor=False):
     i = tf.image.convert_image_dtype(i, tf.float32) # uint8 [0, 255] -> float32 [0, 1]
     if as_4d_tensor:
         i = i[tf.newaxis, ...]
-        
+    # i = tf.image.resize(i, config['styleImageSize']) 
     return i
 
 def write_image(image_path, image):
+    # image = tf.image.resize(image, config['contentImageSize'])
     i = tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
     i = tf.image.convert_image_dtype(i, tf.uint8) # float32 [0, 1] -> uint8 [0, 255]
     i = tf.image.encode_jpeg(i)
